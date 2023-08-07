2f34847bf305d66c0e2e84322f6c569b6d7d7d18
sagar442-debug <bnradlyp1@gmail.com>
2023-08-07 22:08:56 +0545

    first commit

---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+ 
+
+
+const Trending = () => {
+  return (
+    <div>
+      Trending
+    </div>
+  )
+}
+
+export default Trending
