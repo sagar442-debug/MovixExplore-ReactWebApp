89aa5ac290c387a1d06f3d3da8f42a91ecfacc51
sagar442-debug <bnradlyp1@gmail.com>
2023-08-08 18:42:17 +0545

    Adding multiple features such as details page for the movies clicked and using multiple api calls for all the details required in the details page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { fetchDataFromApi } from './utils/api'
 import { useSelector, useDispatch } from 'react-redux'
-import { getApiConfiguration } from './store/homeSlice'
+import { getApiConfiguration, getGenres } from './store/homeSlice'
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
 import Home from "./pages/home/Home"
@@ -20,23 +20,39 @@ function App() {
 
   useEffect(() => {
     fetchApiConfig();
+    genresCall();
   }, [])
 
   const fetchApiConfig = () => {
     fetchDataFromApi('/configuration')
       .then((res) => {
-        console.log(res)
 
         const url = {
           backdrop: res.images.secure_base_url + "original",
           poster: res.images.secure_base_url + "original",
           profile: res.images.secure_base_url + "original",
-
         }
-
         dispatch(getApiConfiguration(url))
       })
   }
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+        promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    data.map(({ genres }) => {
+        return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+};
+
 
   return (
     <BrowserRouter>
--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -12,31 +12,42 @@ import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
 import "./style.scss";
 import CircleRating from "../circleRating/CircleRating";
+import Genres from "../genres/Genres";
 
 
 
-const Carousel = ({ data, loading }) => {
+const Carousel = ({ data, loading, endPoint }) => {
+    console.log(endPoint)
 
     const carouselContainer = useRef();
     const { url } = useSelector((state) => state.home)
     const navigate = useNavigate();
 
     const navigation = (dir) => {
+        const container = carouselContainer.current;
 
-    }
+        const scrollAmount =
+            dir === "left"
+                ? container.scrollLeft - (container.offsetWidth + 20)
+                : container.scrollLeft + (container.offsetWidth + 20);
+
+        container.scrollTo({
+            left: scrollAmount,
+            behavior: "smooth",
+        });
+    };
 
     const skItem = () => {
         return (
-            <div className="skeletonItems">
-                <div className="posterBlock skeleton">
-                    <div className="textBlock">
-                        <div className="title skeleton"></div>
-                        <div className="date skeleton"></div>
-                    </div>
+            <div className="skeletonItem">
+                <div className="posterBlock skeleton"></div>
+                <div className="textBlock">
+                    <div className="title skeleton"></div>
+                    <div className="date skeleton"></div>
                 </div>
             </div>
-        )
-    }
+        );
+    };
 
     return (
         <div className="carousel">
@@ -50,14 +61,15 @@ const Carousel = ({ data, loading }) => {
                     onClick={() => navigation("right")}
                 />
                 {!loading ?
-                    <div className="carouselItems">
+                    (<div className="carouselItems" ref={carouselContainer}>
                         {data?.map((item) => {
                             const posterUrl = item.poster_path ? url.poster + item.poster_path : PosterFallback;
                             return (
-                                <div key={item.id} className="carouselItem">
+                                <div key={item.id} className="carouselItem" onClick={()=> navigate(`/${item.media_type || endPoint}/${item.id}`)}>
                                     <div className="posterBlock">
                                         <Img src={posterUrl} />
                                         <CircleRating rating={item.vote_average.toFixed(1)} />
+                                        <Genres data={item.genre_ids.slice(0, 2)} />
                                     </div>
                                     <div className="textBlock">
                                         <span className="title">
@@ -70,15 +82,15 @@ const Carousel = ({ data, loading }) => {
                                 </div>
                             )
                         })}
-                    </div>
+                    </div>)
                     :
-                    <div className="loadingSkeleton">
+                    (<div className="loadingSkeleton">
                         {skItem()}
                         {skItem()}
                         {skItem()}
                         {skItem()}
                         {skItem()}
-                    </div>
+                    </div>)
                 }
             </ContentWrapper>
         </div>
